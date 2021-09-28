6ded6804b020d57e1fb01e72f695dcad04351a86
Michael Shklyar <michael.shklyar@instructure.com>
2021-09-28 19:35:50 +0300

    task01

---
+++ tasks/task01/task1.py
@@ -0,0 +1,11 @@
+for i in range(1, 1000):
+    b = []
+    mem = i
+    for x in [100, 10, 1]:
+        b.append(i // x)
+        i %= x
+    t = 0
+    for z in b:
+        t = t + (z ** len(b))
+    if mem == t:
+        print(t)
