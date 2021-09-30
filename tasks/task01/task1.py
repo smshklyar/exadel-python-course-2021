d4b53d3d63764a119ed253be67f47ba4c04d921b
Michael Shklyar <michael.shklyar@instructure.com>
2021-09-30 17:06:51 +0300

    fix

--- tasks/task01/task1.py
+++ tasks/task01/task1.py
@@ -6,7 +6,7 @@ for i in range(1, 1000):
         b.append(i // x)
         i %= x
     t = 0
-    for z in b:
-        t = t + (z ** (int(math.log10(mem))+1))
+    lensq = int(math.log10(mem))+1
+    t = sum([z**lensq for z in b])
     if mem == t:
         print(t)
