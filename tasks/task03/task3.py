4938c419ff1e48dc8fd2b833813c160a9c263e0d
Michael Shklyar <michael.shklyar@instructure.com>
2021-10-06 20:04:28 +0300

    fix

--- tasks/task03/task3.py
+++ tasks/task03/task3.py
@@ -7,13 +7,20 @@ texts = [
 al = []
 dictn = {}
 t = 0
+
 for index, line in enumerate(texts):
    for l in line.split():
       al.append(re.sub('[^a-zA-Z]+', '', l.lower()))
       t = t + 1
       dictn[index, re.sub('[^a-zA-Z]+', '', l.lower())] = t
 tp = tuple(dictn)
+dls = {}
 for inx, v in enumerate(tp):
    if inx == 0:
       print("word", "count", "line")
-   print(v[1], al.count(v[1].lower()), v[0])
+   dls[v[1]] = al.count(v[1].lower()), v[0]
+#   print(v[1], al.count(v[1].lower()), v[0])
+for k, f in dls.items():
+   if inx == 0:
+      print(f'{"word"}{"count"}{"line"}')
+   print(f'{k:5}{f[0]:5}{f[1]:5}')
