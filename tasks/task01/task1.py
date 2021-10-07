12ce3fd7bc17f2dc44d5bf5442a283c9eb14ada6
smshklyar <77282927+smshklyar@users.noreply.github.com>
2021-10-07 16:18:42 +0300

    Update task1.py

--- tasks/task01/task1.py
+++ tasks/task01/task1.py
@@ -1,12 +1,10 @@
 import math
-n=1
 for i in range(1, 1000):
     b = []
     mem = i
     while i > 0:
         b.insert(0, i % 10)
         i = i // 10
-    t = 0
     lensq = int(math.log10(mem))+1
     t = sum([z**lensq for z in b])
     if mem == t:
